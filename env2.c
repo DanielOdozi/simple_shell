5510a2587a4db2413787cc5e64498f8452368582
Abdullah <abdallahsuleiman56@gmail.com>
2022-11-15 11:59:45 +0100

    power up

---
+++ env2.c
@@ -0,0 +1,121 @@
+#include "main.h"
+
+/**
+ * copy_info - copeis info tocreate a new env or alias
+ * @name: name (env or alias)
+ * @value: value
+ * Return: new env or alias
+ */
+char *copy_info(char *name, char *value)
+{
+	char *new;
+	int len_name, len_value, len;
+
+	len_name = _strlen(name);
+	len_value = _strlen(value);
+	len = len_name + len_value + 2;
+	new = malloc(sizeof(char) * (len));
+	_strcpy(new, name);
+	_strcat(new, "=");
+	_strcat(new, value);
+	_strcat(new, "\0");
+
+	return (new);
+}
+
+/**
+ * set_env -sets an environment variable
+ * @name: nameof the variable
+ * @value: value of the variable
+ * @datash: data structure for the shell
+ * Return: nothing
+ */
+void set_env(char *name, char *value, data_shell *datash)
+{
+	int i;
+	char *var_env, *name_env;
+
+	for (i = 0; datash->_environ[i]; i++)
+	{
+		var_env = _strdup(datash->_environ[i]);
+		name_env = _strtok(var_env, "=");
+		if (_strcmp(name_env, name) == 0)
+		{
+			free(datash->_environ[i]);
+			datash->_environ[i] = copy_info(name_env, value);
+			free(var_env);
+			return;
+		}
+		free(var_env);
+	}
+
+	datash->_environ = _reallocdp(datash->_environ, i, sizeof(char *) * (i + 2));
+	datash->_environ[i] = copy_info(name, value);
+	datash->_environ[i + 1] = NULL;
+}
+
+/**
+ * _setenv - compares env variable names
+ * @datash: relevant data
+ * Return: 1 on success
+ */
+int _setenv(data_shell *datash)
+{
+	if (datash->args[1] == NULL || datash->args[2] == NULL)
+	{
+		get_error(datash, -1);
+		return (1);
+	}
+
+	set_env(datash->args[1], datash->args[2], datash);
+
+	return (1);
+}
+
+/**
+ * _unsetenv - deletes an environmental variable
+ * @datash: relevant data
+ * Return: 1 on success
+ */
+int _unsetenv(data_shell *datash)
+{
+	char **realloc_environ;
+	char *var_env, *name_env;
+	int i, j, k;
+
+	if (datash->args[1] == NULL)
+	{
+		get_error(datash, -1);
+		return (1);
+	}
+	k = -1;
+	for (i = 0; datash->_environ[i]; i++)
+	{
+		var_env = _strdup(datash->_environ[i]);
+		name_env = _strtok(var_env, "=");
+		if (_strcmp(name_env, datash->args[1]) == 0)
+		{
+			k = i;
+		}
+		free(var_env);
+	}
+	if (k == -1)
+	{
+		get_error(datash, -1);
+		return (1);
+	}
+	realloc_environ = malloc(sizeof(char *) * (i));
+	for (i = j = 0; datash->_environ[i]; i++)
+	{
+		if (i != k)
+		{
+			realloc_environ[j] = datash->_environ[i];
+			j++;
+		}
+	}
+	realloc_environ[j] = NULL;
+	free(datash->_environ[k]);
+	free(datash->_environ);
+	datash->_environ = realloc_environ;
+	return (1);
+}
---
+++ exec_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * exec_line - finde builtins and commands
+ * @datash: relevant data
+ * Return: 1 on success
+ */
+int exec_line(data_shell *datash)
+{
+	int (*builtin)(data_shell *datash);
+
+	if (datash->args[0] == NULL)
+		return (1);
+
+	builtin = get_builtin(datash->args[0]);
+
+	if (builtin != NULL)
+		return (builtin(datash));
+
+	return (cmd_exec(datash));
+}
